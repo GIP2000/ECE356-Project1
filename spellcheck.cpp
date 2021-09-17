16d512fbbaea90ea03f7918f6f3739648ad7a741
Gregory Presser <gpress2222@gmail.com>
2021-09-17 02:50:38 -0400

    Added comments

--- spellcheck.cpp
+++ spellcheck.cpp
@@ -7,14 +7,18 @@
 
 using namespace std;
 
+//Writes error to otuput file if the word is too long 
 void writeLengthError(int lineCount, string word, ofstream& outputDocument){
     outputDocument << "Long word at line " << lineCount << ", starts: " << word.substr(0,20) << "\n"; 
 }
 
+//Writes error to the output file if the word is not int he dictionary 
 void writeWordNotFound (int lineCount, string word, ofstream& outputDocument){
     outputDocument << "Unknown word at line " << lineCount << ": " << word << "\n"; 
 }
 
+
+// converts a string word to all lower case
 void toLowerCase(string& word){
     for(int i = 0; i<word.length(); i++){
         if(isupper(word[i]))
@@ -22,7 +26,7 @@ void toLowerCase(string& word){
     }
 }
 
-
+//returns dictionary (in hash table ) from a file where every line is an entery in the hash table
 hashTable buildDictionary(){
     hashTable table(0); 
     cout << "What is the path of dictionary file\n";
@@ -44,7 +48,8 @@ hashTable buildDictionary(){
 }
 
 
-
+// checks if an individual word is valid.
+// If not it will use one of the above error funcitons to indicate so
 void checkWord (hashTable dictionary, string word, int lineCount, ofstream& outputDocument){
     toLowerCase(word);
     if(word.size() > 20) return writeLengthError(lineCount,word, outputDocument);
@@ -52,13 +57,14 @@ void checkWord (hashTable dictionary, string word, int lineCount, ofstream& outp
     return writeWordNotFound(lineCount,word, outputDocument); 
 }
 
+// seperates an input document into valid words and checks if the word is valid or not
 void sepAndCheckWords(hashTable dictionary,string documentName, string outputDocumentName){
     auto start = chrono::steady_clock::now(); 
     ifstream document(documentName);
     ofstream outputDocument(outputDocumentName); 
     if(document.is_open()){
         string line; 
-        regex r("([\\w'-]+)");  
+        regex r("([\\w'-]+)");
         int lineCount = 1;
         while(getline(document,line)){
             smatch match; 
@@ -74,6 +80,7 @@ void sepAndCheckWords(hashTable dictionary,string documentName, string outputDoc
     cout << "Time it took to spell check file is " << timeInSeconds.count() << "s\n"; 
 }
 
+// gets the input and otuput file names and initates spell check 
 void spellCheck(hashTable dictionary){
     cout << "What is the name of the spellcheck document\n"; 
     string documentName,outputDocumentName;
